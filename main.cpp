095ce4f1c86990fbca86f1087fa166a450d4c13f
HARSH-MADHAVAN <harshmadhavan2003@gmail.com>
2024-12-06 17:00:09 +0530

    Created a Game Window

--- main.cpp
+++ main.cpp
@@ -8,14 +8,22 @@ int main () {
     cout << "Starting the Game" << endl;
 
     const int screen_width = 1280;
-    const int screen_height = 1280;
+    const int screen_height = 800;
 
     // InitWindow(width, height, title)
-    // function to create a window
-    InitWindow(screen_width, screen_height, "Ping Pong!!")
+    InitWindow(screen_width, screen_height, "Ping Pong!!"); // function to create a window
+    SetTargetFPS(60); // defines the speed at which the game will work, if not defined it will work according to your computers speed.
 
+    while (WindowShouldClose() == false) // this function returns true is the esc button is pressed of the window is closed
+    {
+        BeginDrawing();
 
+        EndDrawing();
+    }
+    
 
     CloseWindow(); // function to destroy the window
     return 0;
 }
+
+// To run the game press F5
