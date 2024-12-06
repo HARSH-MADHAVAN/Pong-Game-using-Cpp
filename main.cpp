c59d61104f5825bb35f2e598f74088cd6137566b
HARSH-MADHAVAN <harshmadhavan2003@gmail.com>
2024-12-06 16:49:02 +0530

    Created a Game Window

--- main.cpp
+++ main.cpp
@@ -6,4 +6,16 @@ using namespace std;
 int main () {
 
     cout << "Starting the Game" << endl;
+
+    const int screen_width = 1280;
+    const int screen_height = 1280;
+
+    // InitWindow(width, height, title)
+    // function to create a window
+    InitWindow(screen_width, screen_height, "Ping Pong!!")
+
+
+
+    CloseWindow(); // function to destroy the window
+    return 0;
 }
