6e784b3919ea2a99ed95bd9573ea882e78fd9b80
HARSH-MADHAVAN <harshmadhavan2003@gmail.com>
2024-12-06 16:42:14 +0530

    Resolved merge conflicts in README.md and main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-<<<<<<< HEAD
 #include <raylib.h>
 
 using namespace std;
@@ -7,12 +6,4 @@ using namespace std;
 int main () {
 
     cout << "Starting the Game" << endl;
-=======
-using namespace std;
-
-int main() {
-    cout << "Hello World" << endl;
-
-    return 0;
->>>>>>> 848ba02d4c452a139dd8d2fac614747f24f75fb6
 }
