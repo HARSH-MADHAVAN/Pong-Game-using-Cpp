c63d8699e0696380024cbbd1ed548f3738cf2bc3
HARSH-MADHAVAN <harshmadhavan2003@gmail.com>
2024-12-10 23:16:47 +0530

    Animating the ball

--- main.cpp
+++ main.cpp
@@ -3,6 +3,25 @@
 
 using namespace std;
 
+class Ball{
+public:
+    float x, y;
+    int speed_x, speed_y;
+    int radius;
+
+void Draw() {
+    DrawCircle(x, y, radius, WHITE);
+}
+
+void Update() {
+    x += speed_x;
+    y += speed_y;
+}
+
+};
+
+Ball ball;
+
 int main () {
 
     cout << "Starting the Game" << endl;
@@ -15,14 +34,26 @@ int main () {
     InitWindow(screen_width, screen_height, "Ping Pong!!"); 
     SetTargetFPS(60); // defines the speed(number of frames per second) at which the game will work, if not defined it will work according to your computers speed.
 
-// Game Loop(Step 1)
+    ball.radius = 20;
+    ball.x = screen_width / 2;
+    ball.y = screen_height / 2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+
+    // Game Loop(Step 1)
     while (WindowShouldClose() == false) // this function returns true is the esc button is pressed of the window is closed
     {
         BeginDrawing();
-// Drawing the center line
+
+        // Updating
+        ball.Update();
+
+        
+        // Drawing the center line
         DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
-// Drawing Ball
-        DrawCircle(screen_width / 2, screen_height / 2, 20, WHITE);
+        ball.Draw();
+        // Drawing Ball
+        // DrawCircle(screen_width / 2, screen_height / 2, 20, WHITE);
 // Drawing player rectangles
         DrawRectangle(10, screen_height / 2 - 60, 25,120, WHITE);
         DrawRectangle(screen_width - 35, screen_height / 2 - 60, 25, 120, WHITE);
